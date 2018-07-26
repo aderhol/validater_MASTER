90d233f25b3eed1110c5bfa171b6c88977f1be60
Adam <a.e.hollos@gmail.com>
2018-07-26 23:43:49 +0200

    METRICS: altitude, DOP, maxSNR, avrSNR, loc

---
+++ validater_MASTER/Debug/waveform.d
@@ -0,0 +1,29 @@
+# FIXED
+
+waveform.obj: ../waveform.c
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/math.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_ti_config.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/linkage.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_defs.h
+waveform.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_limits.h
+
+../waveform.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/math.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_defs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_limits.h: 
---
+++ validater_MASTER/main.c
@@ -0,0 +1,94 @@
+/* XDCtools Header files */
+#include <xdc/std.h>
+#include <xdc/runtime/System.h>
+
+/* BIOS Header files */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/knl/Clock.h>
+#include <ti/sysbios/knl/Semaphore.h>
+
+/* TI-RTOS Header files */
+#include <ti/drivers/GPIO.h>
+#include <ti/drivers/UART.h>
+
+/* Example/Board Header files */
+#include "Board.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <bLogic.h>
+
+#define PERIOD_MS 1000
+
+#define TASKSTACKSIZE     5120
+
+static int64_t time = 788125453;//-1;
+
+Task_Struct task0Struct;
+Char task0Stack[TASKSTACKSIZE];
+/*
+ *  ======== echoFxn ========
+ *  Task for this function is created statically. See the project's .cfg file.
+ */
+Semaphore_Handle sem;
+Void echoFxn(UArg arg0, UArg arg1)
+{
+    while (1) {
+        Semaphore_pend(sem, BIOS_WAIT_FOREVER);
+        sendPacket(time);
+    }
+}
+
+Void timerFxn(UArg arg0)
+{
+    time++;
+    Semaphore_post(sem);
+    GPIO_toggle(Board_sync);
+}
+
+/*
+ *  ======== main ========
+ */
+int main(void)
+{
+    /* Call board init functions */
+    Board_initGeneral();
+    Board_initGPIO();
+    Board_initUART();
+
+    /* Construct BIOS objects */
+    Task_Params taskParams;
+
+    Task_Params_init(&taskParams);
+    taskParams.stackSize = TASKSTACKSIZE;
+    taskParams.stack = &task0Stack;
+    taskParams.instance->name = "echo";
+    Task_construct(&task0Struct, (Task_FuncPtr)echoFxn, &taskParams, NULL);
+
+
+    Semaphore_Params smP;
+    Semaphore_Params_init(&smP);
+    smP.mode = Semaphore_Mode_BINARY;
+    sem = Semaphore_create( 0, &smP, NULL );
+
+    Clock_Params clkPr;
+    Clock_Params_init(&clkPr);
+    clkPr.arg = NULL;
+    clkPr.period = PERIOD_MS;
+    clkPr.startFlag = 1;
+    Clock_create(timerFxn, 1000, &clkPr, NULL);
+
+    bLogic_Init();
+
+    System_printf("Master validator is now in operation.");
+    /* SysMin will only print to the console when you call flush or exit */
+    System_flush();
+
+    /* Start BIOS */
+    BIOS_start();
+
+    return (0);
+}
